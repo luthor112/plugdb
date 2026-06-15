c1298ac2308222a1b36ec5d6e6264666fbfd0200
Ethan Alexander <81448108+shdwmtr@users.noreply.github.com>
2026-06-15 13:31:04 -0300

    chore: move plugin store

--- scripts/build/upload-dist.sh
+++ scripts/build/upload-dist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "::group::Database"
+echo "::group::Upload"
 
 PLUGIN_NAME=$1
 
@@ -7,14 +7,13 @@ cd dist
 
 echo "Building plugin archive..."
 zip -r "$PLUGIN_NAME.zip" .
-
 echo "Successfully built plugin."
 
 id=$(jq -r '.id' "$PLUGIN_NAME"/metadata.json) || { echo "::error::Failed to extract id from $PLUGIN_NAME/metadata.json"; exit 1; }
 
-echo "Uploading plugin to database with id $id"
-gsutil cp "$PLUGIN_NAME.zip" gs://millennium-d9ce0.appspot.com/plugins/"$id.zip"
-echo "Successfully uploaded plugin to database."
+echo "Uploading plugin with id $id"
+scp "$PLUGIN_NAME.zip" ubuntu@"$SSH_HOST":~/net/storage/steambrew/plugins/"$id.zip"
+echo "Successfully uploaded plugin."
 rm "$PLUGIN_NAME.zip"
 
 echo "::endgroup::"
